bb8cc738e464d5c7a7899fe66ed9fcda40b6e117
ramo-dev <annuar.dev@gmail.com>
2024-06-09 12:54:27 +0300

    added cors

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const dotenv = require("dotenv");
 const { sendMail } = require("./Mail/Email.js");
 // const {randomChars} = require("./anonymous.js");
 
-
 // env variables configuration
 dotenv.config();
 
@@ -13,30 +12,29 @@ const app = express();
 
 // json parse middlewares
 app.use(express.json());
-app.use(cors())
-
+app.use(cors());
 
 // server routes
-
-
-
 
 app.post("/", async (req, res) => {
   const { fullName, email, message } = req.body;
   try {
     await sendMail(fullName, email, message);
-    res.status(200).send("Email sent successfully, you will receive a confirmation email shortly");
+    res
+      .status(200)
+      .send(
+        "Email sent successfully, you will receive a confirmation email shortly"
+      );
   } catch (err) {
     res.status(500).send("Failed to send email");
     console.error(err);
   }
 });
 
-
 app.get("/wakeup", (req, res) => {
-    res.status(200).send("Im up");
+  res.status(200).send("Im up");
+  console.log("waking up");
 });
-
 
 // start server
 app.listen(3000, () => {
